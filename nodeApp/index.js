5b86ddb940f54759aab52342224969f69d3c35f6
MoonWalker <silavsale@gmail.com>
2023-08-15 14:42:30 -0400

    add go app

--- nodeApp/index.js
+++ nodeApp/index.js
@@ -3,7 +3,7 @@ import express from "express"
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send("Hello World!")
+    res.send("Hello World!!! and Mars!!!")
 })
 
 app.listen(3000, ()=> {
